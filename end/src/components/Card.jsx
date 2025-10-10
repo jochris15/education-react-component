ece2aadbb271aedfbc0feea910a8945299849852
jochris15 <jochris15@gmail.com>
2025-10-10 10:12:51 +0700

    update prop

--- end/src/components/Card.jsx
+++ end/src/components/Card.jsx
@@ -1,18 +1,18 @@
-export default function Card({ el, index }) {
+export default function Card({ product, index }) {
     return (
         <>
-            <div className={index % 2 === 0 ? "bg-yellow-400 h-full flex flex-col justify-center items-center border-2 border-black p-5 rounded-2xl shadow-[2px_2px_0px_rgba(0,0,0,1)]" : "bg-blue-400 h-full flex flex-col justify-center items-center border-2 border-black p-5 rounded-2xl shadow-[2px_2px_0px_rgba(0,0,0,1)]"} key={el.id}>
+            <div className={index % 2 === 0 ? "bg-yellow-400 h-full flex flex-col justify-center items-center border-2 border-black p-5 rounded-2xl shadow-[2px_2px_0px_rgba(0,0,0,1)]" : "bg-blue-400 h-full flex flex-col justify-center items-center border-2 border-black p-5 rounded-2xl shadow-[2px_2px_0px_rgba(0,0,0,1)]"} key={product.id}>
                 <div className="flex flex-1">
                     <img
-                        src={el.imgUrl}
+                        src={product.imgUrl}
                         alt="product image"
                         className="border-2 border-black rounded-2xl shadow-[2px_2px_0px_rgba(0,0,0,1)]"
                     />
                 </div>
                 <div className="flex flex-col divide-y divide-black">
-                    <b className="mt-5">{el.name}</b>
+                    <b className="mt-5">{product.name}</b>
                     <p className="line-clamp-4">
-                        {el.description}
+                        {product.description}
                     </p>
                 </div>
             </div>
--- end/src/components/HomePage.jsx
+++ end/src/components/HomePage.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react"
-import json from "../assets/product.json"
+import productJson from "../assets/product.json"
 import Navbar from "./Navbar"
 import Card from "./Card"
 
 export default function HomePage({ setPage }) {
-    const [products, setProducts] = useState(json)
+    const [products, setProducts] = useState(productJson)
 
     return (
         <>
             <Navbar setPage={setPage} />
             <div id="PAGE-HOME" className="min-h-screen flex items-center justify-center">
                 <main className="my-5 bg-white grid grid-cols-4 gap-5">
-                    {products.map((el, index) => {
-                        return <Card el={el} index={index} key={el.id} />
+                    {products.map((product, index) => {
+                        return <Card product={product} index={index} key={product.id} />
                     })}
                 </main>
             </div>
